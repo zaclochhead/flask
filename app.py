58d6a29b599b2498457d7c08bea271e5d486b3b7
zac <zaclochhead@gmail.com>
2019-03-03 00:18:09 +1100

    another page

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 @app.route("/salvador")
 def salvador():
     return "Hello, Salvador"
